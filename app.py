476c0ea6c5dbf82e6d620da7ece5ef606760db79
honeybadger26 <honeybadger26@gmail.com>
2021-02-27 00:01:06 +1100

    improved console output

--- app.py
+++ app.py
@@ -5,20 +5,21 @@ from scrapers import elsevier, sage, springer
 from links import LINKS
 
 # TODO: Error handling
-# TODO: Nicer output: https://stackoverflow.com/a/5291044
 
 def main():
     if not os.path.exists('out'):
         os.makedirs('out')
     
     filename = os.path.join('out/', '%s.csv' % datetime.now().strftime('%Y.%m.%d_%H.%M.%S'))
-    csv_columns = ['Name', 'E-mail', 'Title', 'Journal Title', 'Search Link']
+    csv_columns = ['Name', 'Title', 'Journal Title', 'Search Link']
 
     csvfile = open(filename, 'w', newline='', encoding='utf-8')
     writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
     writer.writeheader()
 
-    for link in LINKS:
+    for idx, link in enumerate(LINKS):
+        print('LINK %d OF %d' % (idx+1, len(LINKS)))
+
         if 'www.springer.com' in link:
             springer.scrape(link, writer)
         elif 'journals.sagepub.com' in link:
@@ -26,8 +27,9 @@ def main():
         elif 'www.elsevier.com' in link:
             elsevier.scrape(link, writer)
         else:
-            print('>> ERROR: INCORRECT LINK')
+            print('ERROR: INCORRECT LINK')
 
+    print('DONE')
     csvfile.close()
 
 if __name__ == '__main__':
